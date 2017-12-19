66e054979ca819c8bd5ed3be91f8fe13c18c3fea
leadensky <ben@leadensky.com>
2017-12-19 07:38:02 -0700

    Initial upload
    Added first version files.

---
+++ bowling/Bowling.py
@@ -0,0 +1,73 @@
+class Frame:
+    number = 0
+    first = None
+    second = None
+    third = None
+
+    def __init__(self, frameNumber):
+        self.number = frameNumber
+
+class Game:
+    frames = []
+
+    def __init__(self):
+        i = 1
+        while i < 11:
+            print (i)
+            self.frames.append(Frame(i))
+            i = i + 1
+    
+    def calculate(self):
+        score = 0
+        i = 1
+        while i < 11:
+            if i < 10:
+                if self.frames[i - 1].first != None:
+                    score = score + self.frames[i].first
+
+                if self.frames[i - 1].second != None:
+                    score = score + self.frames[i].second
+
+            i = i + 1
+
+        print(score)
+
+    def getScore(self, frameNumber, ball):
+            print("---Frame " + str(frameNumber) + "---")
+            value = int(input("Enter # of pins " + ball + " ball: "))
+            if value == 10:
+                print("STRIKE!")
+
+            return value
+
+    def play(self):
+        i = 1
+        while i < 11:
+            first = self.getScore(i, "first")
+            #print("---Frame " + str(i) + "---")
+            #first = int(input("Enter # of pins first ball: "))
+            self.frames[i - 1].first = first
+
+            if first < 10:
+                second = self.getScore(i, "second")                
+                while(second > 10 - first):
+                    print("***ERROR: Second ball must be less than or equal to: " + str(10 - first))
+                    second = self.getScore(i, "second")               
+
+                if first + second == 10:
+                    print("SPARE!")
+
+                self.frames[i - 1].second = second
+
+            if i == 10:
+                if first == 10:
+                    second = self.getScore(i, "second")                
+                    self.frames[i - 1].second = second
+                    
+                    third = self.getScore(i, "third")
+                    self.frames[i - 1].third = third
+
+            i = i + 1
+
+game = Game()
+game.play()
---
+++ bowling/Bowling2.py
@@ -0,0 +1,122 @@
+class Game:
+    def __init__(self):
+        self.itsScorer = Scorer()
+
+    def score(self):
+        return self.scoreForFrame(self.itsCurrentFrame)
+
+    def add(self, pins):
+        self.itsScorer.addThrow(pins)
+        self.adjustCurrentFrame(pins);
+
+    def adjustCurrentFrame(self, pins):
+        if(self.firstThrowInFrame == True):
+            if(self.adjustFrameForStrike(pins) == False):
+                self.firstThrowInFrame = False
+            else:
+                self.firstThrowInFrame = True
+                self.advanceFrame()
+
+    def adjustFrameForStrike(self, pins):
+        if(pins == 10):
+            self.advanceFrame()
+            return True
+        
+        return False
+
+    def advanceFrame(self):
+        self.itsCurrentFrame = min([10, self.itsCurrentFrame + 1])
+
+    def scoreForFrame(self, theFrame):
+        return self.itsScorer.scoreForFrame(theFrame)
+
+    itsCurrentFrame = 0
+    firstThrowInFrame = True
+    itsScorer = None
+
+class Scorer:
+    ball = 0
+    itsThrows = [0] * 21
+    itsCurrentThrow = 0
+
+    def addThrow(self, pins):
+        self.itsThrows[self.itsCurrentThrow] = pins
+        self.itsCurrentThrow += 1
+
+    def scoreForFrame(self, theFrame):
+        ball = 0
+        score = 0
+        for currentFrame in range(0, theFrame):
+            if(self.strike()):
+                score += 10 + self.nextTwoBalls()
+            elif(self.spare()):
+                score += 10 + self.nextBall()
+            else:
+                score += self.twoBallsInFrame()
+
+        return score
+
+    def strike(self):
+        if(self.itsThrows[self.ball] == 10):
+            self.ball += 1
+            return True
+
+        return False
+
+    def spare(self):
+        if(self.itsThrows[self.ball] + self.itsThrows[self.ball+1] == 10):
+            self.ball += 1
+            return True
+
+        return False
+
+    def nextTwoBalls(self):
+        return self.itsThrows[self.ball] + self.itsThrows[self.ball + 1]
+
+    def nextBall(self):
+        return self.itsThrows[self.ball]
+
+    def twoBallsInFrame(self):
+        score = self.itsThrows[self.ball]
+        self.ball += 1
+        score += self.itsThrows[self.ball]
+        self.ball +=1
+        return score
+
+    #Tests (sort of)
+def bowlPerfectGame():
+    game = Game()
+    for i in range(0, 12):
+        game.add(10)
+    
+    print (game.scoreForFrame(10))
+
+def bowl299Game():
+    game = Game()
+    for i in range(0, 11):
+        game.add(10)
+
+    game.add(9)
+    print (game.scoreForFrame(10))
+
+def bowl279Game():
+    game = Game()
+    game.add(9)
+    game.add(0)
+    for i in range(0,11):
+        game.add(10)
+
+    print (game.scoreForFrame(10))
+
+def bowl90Game():
+    game = Game()
+    for i in range(0, 10):
+        game.add(9)
+        game.add(0)
+
+    print(game.scoreForFrame(10))
+
+bowlPerfectGame()
+bowl299Game()
+bowl279Game()
+bowl90Game()
